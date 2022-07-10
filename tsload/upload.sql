086bdad530c3b3a88f218d6491ec04ac84c648de
= <deeekaay@outlook.com>
2022-07-10 20:21:52 +0000

    Renames mktdata columns.

--- tsload/upload.sql
+++ tsload/upload.sql
@@ -1,4 +1,4 @@
-\copy tmp_mktdata  (id,q,t,p,r,ts,region) from '/srv/tsload/tmp2.csv' delimiter ',' CSV;
-insert into mktdata (id,q,t,p,r,ts,region)
-	select id, q,t,p,r,to_timestamp(ts),region from tmp_mktdata;
+\copy tmp_mktdata  (item_id,quantity,total,price,r,ts,region) from '/srv/tsload/tmp2.csv' delimiter ',' CSV;
+insert into mktdata (item_id,quantity,total,price,r,ts,region)
+	select item_id,quantity,total,price,r,to_timestamp(ts),region from tmp_mktdata;
 truncate table tmp_mktdata;
